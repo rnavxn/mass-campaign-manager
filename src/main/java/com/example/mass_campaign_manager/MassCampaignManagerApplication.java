98bb55065a667bc484a77ae74c9197045bf1919b
Navin <rnavxn@gmail.com>
2026-05-04 17:49:04 +0530

    Initial Commit

---
+++ src/main/java/com/example/mass_campaign_manager/MassCampaignManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mass_campaign_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassCampaignManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassCampaignManagerApplication.class, args);
+	}
+
+}
